62e59c6f17a8abf610ad66c66930d1a71ba98b39
0Range <23374351+orangefrg@users.noreply.github.com>
2017-10-24 22:55:58 +0300

    AJAX tests

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
-from ewhouse.views import getter
+from ewhouse.views import getter, all_items
 
 admin.autodiscover()
 
-urlpatterns = [url(r'^getter/$', getter, name='getter')]
+urlpatterns = [url(r'^getter/$', getter, name='getter'),
+               url(r'^units/$', all_items, name='all_items')]
--- views.py
+++ views.py
@@ -1,7 +1,22 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Unit
+import simplejson
+
 
 @csrf_exempt
 def getter(request):
-    json_string = request.body.decode("utf-8")
+    json_string = request.body.decode('utf-8')
     return HttpResponse()
+
+@csrf_exempt
+def all_items(request):
+    objs = Unit.objects.all()
+    objs_val = {'objects': list(objs.values('component__name', 'component__component_type__name',
+                           'component__component_type__macro_type__name','where_from__name',
+                           'price','price_currency__symbol','location__name',
+                           'location__warehouse__name','current_count'))}
+    ret = JsonResponse(objs_val)
+    ret['Access-Control-Allow-Origin'] = '*'
+    ret['charset'] = 'utf-8'
+    return ret
